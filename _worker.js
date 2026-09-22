2bbce067dceb8d264446298c692836232c72a56f
wzhxll <3974338461@qq.com>
2026-09-22 12:33:49 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,14 +1,14 @@
 export default {
   async fetch(req, env) {
     const u = new URL(req.url);
-    const key = u.searchParams.get("key"); // 读取url上的key参数
-    const ua = req.headers.get("user-agent")||""; // 获取访问者UA标识
+    const key = u.searchParams.get("key");
+    const ua = req.headers.get("user-agent")||"";
 
-    // 判断条件：密钥不对 【或者】是浏览器(Mozilla)，直接跳转B站
     if(key !== "WillowLeaf6688" || ua.includes("Mozilla")){
       return Response.redirect("https://b23.tv/MRnB7hi");
     }
-    // 密钥正确 + 不是浏览器 → 返回你的lua源码
-    return env.ASSETS.fetch(req);
+    u.search = "";
+    const newReq = new Request(u, req);
+    return env.ASSETS.fetch(newReq);
   }
 }
