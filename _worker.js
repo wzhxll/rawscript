904f8e73bd94c49d3241b37e65f0eaa97a484c5c
wzhxll <3974338461@qq.com>
2026-09-22 11:34:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,12 @@
 export default {
   async fetch(request, env, ctx) {
+    const url = new URL(request.url);
     const KEY = "柳叶Willow leaf";
-    const reqKey = request.headers.get("X-Secret-Key");
+    const inputKey = url.searchParams.get("key");
 
-    if (reqKey !== KEY) {
-      // 无密钥，直接重定向rickroll链接
+    // 密钥匹配校验
+    if (inputKey !== KEY) {
+      // 无密钥/密钥错误，跳转B站链接
       return Response.redirect("https://b23.tv/MRnB7hi", 302);
     }
 
