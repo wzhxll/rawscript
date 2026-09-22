c1bf44b6df8c39d6c2f0c0eac90a903ca3ac5388
wzhxll <3974338461@qq.com>
2026-09-22 10:35:16 +0800

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,10 @@
+export default {
+  async fetch(request, env, ctx) {
+    const ua = request.headers.get("user-agent") || "";
+    const isBrowser = /Chrome|Firefox|Safari|Edge|Opera/i.test(ua);
+    if(isBrowser){
+      return new Response("Forbidden", {status:403})
+    }
+    return env.ASSETS.fetch(request);
+  }
+}
