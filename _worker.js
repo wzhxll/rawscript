fb399a762c391a8b50c2fe4708d43af607db24f6
wzhxll <3974338461@qq.com>
2026-09-22 12:42:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,10 @@ export default {
     const ua = req.headers.get("user-agent")||"";
 
     if(key !== "WillowLeaf6688" || ua.includes("Mozilla")){
-      return Response.redirect("https://b23.tv/MRnB7hi");
+      return new Response('<h1 style="color:red">gay</h1>', {
+        status: 200,
+        headers: {"Content-Type":"text/html"}
+      });
     }
     u.search = "";
     const newReq = new Request(u, req);
