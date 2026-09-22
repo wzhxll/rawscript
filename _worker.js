e39e2ec344a3624494393827c7b0a757e104c9da
wzhxll <3974338461@qq.com>
2026-09-22 12:46:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,13 +5,18 @@ export default {
     const ua = req.headers.get("user-agent")||"";
 
     if(key !== "WillowLeaf6688" || ua.includes("Mozilla")){
-      return new Response('<h1 style="color:red">gay</h1>', {
-        status: 200,
-        headers: {"Content-Type":"text/html"}
-      });
+      // 浏览器访问 → 返回带红色样式的HTML
+      if(ua.includes("Mozilla")){
+        return new Response('<h1 style="color:red">gay</h1>', {
+          headers: {"Content-Type":"text/html"}
+        });
+      }
+      // 注入器请求校验失败，返回纯文本，**没有任何HTML标签**，不会炸loadstring
+      return new Response("gay", {headers:{"Content-Type":"text/plain"}});
     }
     u.search = "";
     const newReq = new Request(u, req);
     return env.ASSETS.fetch(newReq);
   }
 }
+
