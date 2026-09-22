889b67fe1734e359885499ec7b5dbbb88272cad6
wzhxll <3974338461@qq.com>
2026-09-22 12:54:26 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ export default {
     const ua = request.headers.get("user-agent") || "";
     const isBrowser = /Chrome|Firefox|Safari|Edge|Opera/i.test(ua);
     if(isBrowser){
-      return new Response("Forbidden", {看的是gay})
+      return new Response("Forbidden", {status:403})
     }
     return env.ASSETS.fetch(request);
   }
